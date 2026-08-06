f654c553c1008705705648cffa2faffa45f3ff80
Spandir24 <pandirsana@gmail.com>
2026-08-06 15:14:05 +0530

    extra pts

---
+++ App(ep_4).js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Building 'Namaste Food' app
+/* COMPONENTS INCLUDE:
+
+HEADER
+    -logo
+    - nav items
+BODY
+    -search
+    -restaurantContainer
+        -restaurantCard
+FOOTER
+    -copyright
+    -links
+    -address
+    -contact
+*/
+
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render()
