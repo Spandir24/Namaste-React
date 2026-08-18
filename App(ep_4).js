1386fd26fb8ac01b0e4095d8cb78896dc5bb8e06
Spandir24 <pandirsana@gmail.com>
2026-08-18 12:44:51 +0530

    food app_start

--- App(ep_4).js
+++ App(ep_4).js
@@ -18,6 +18,41 @@ FOOTER
     -contact
 */
 
+//Header component
+const Header = () => {
+  return (
+    <div className="header">
+      {/*LOGO*/}
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent.png"
+          height="100px"
+          width="100px"
+        />
+      </div>
 
-const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render()
+      {/*NAV-ITEMS*/}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// App component
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
