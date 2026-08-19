edcd58316c8cbaf11b5311383b42e26ed855df06
Spandir24 <pandirsana@gmail.com>
2026-08-19 22:39:45 +0530

    modifications in app

--- App(ep_4).js
+++ App(ep_4).js
@@ -270,7 +270,14 @@ const RestaurantCard = (props) => {
   // const {resName, cuisine}= props;        // destructuring done when we manually passing the data
 
   const { resData } = props;
-  const {cloudinaryImg, name, cuisines, rating, delivery_time_minutes, costForTwo  } = resData?.data;
+  const {
+    cloudinaryImg,
+    name,
+    cuisines,
+    rating,
+    delivery_time_minutes,
+    costForTwo,
+    restaurant_id} = resData?.data;
 
   return (
     <div className="res-card">
@@ -290,6 +297,7 @@ const RestaurantCard = (props) => {
       <h5>{rating} stars</h5>
       <h5>{delivery_time_minutes} mins</h5>
       <h5>₹{costForTwo / 100} for two</h5>
+      <h5>{restaurant_id} ID</h5>
 
       {/* <h5 className="stars">4.4 stars</h5>
       <h5 className="time">38 mins</h5> */}
@@ -300,6 +308,15 @@ const RestaurantCard = (props) => {
     </div>
   );
 };
+
+
+
+
+
+
+
+
+
 
 // App component
 const AppLayout = () => {
