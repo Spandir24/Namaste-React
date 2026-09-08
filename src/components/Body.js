7c587d3905cba72ec067726d290b4ac05cec83b9
Spandir24 <pandirsana@gmail.com>
2026-09-08 13:01:27 +0530

    food_app(filtering search bar)

--- src/components/Body.js
+++ src/components/Body.js
@@ -49,8 +49,8 @@ const Body = () => {
   // Now we understand 'React Hooks': 1) useState()-
   //LOCAL STATE VARIABLE- (super powerful react variable): we use REACT HOOK (useState)
   const [listOfRestaurants, setlistOfRestaurants] = useState([]);
-
-  const[searchText, setSearchText]= useState("");
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   //above line is doing DESTRUCTURING- (wiz simplified below)
   /*const arr = useState(resList);
@@ -78,7 +78,10 @@ const Body = () => {
       console.log(json);
 
       setlistOfRestaurants(json.data.cards[2].data.data.cards); // here we display the received data on screen =>>>>>>> BUT THIS IS NOT A GOOD WAY OF WRITING CODE  =>>>>>> Thus, we can do 'OPTIONAL CHAINING'
-      setlistOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+      setlistOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);       // we keep this unchanged
+      setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);        // we filter acc to our need, display it on UI and refresh this after every page reload/ component re-render
+
+
     } catch (error) {
       console.log("Fetch failed:", error);
     }
@@ -119,51 +122,62 @@ const Body = () => {
   */
 
   //OR Another way to write the above situation(ie., displaying shimmer effect or filtered data with rating > 4.4 ) w/o using 2 return sttaements is via-: TERNARY operator
-  return listOfRestaurants.length===0 ? (<Shimmer />) : (
-  <div className="body">
-    <div className="filter">
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        {/* Category search button */}
+        <div className="Search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              //searchText
+              console.log(searchText);
 
-      {/* Category search button */}
-      <div className="Search">
-        <input type="text" className="search-box" value={searchText} onChange={ (e) => {
-          setSearchText(e.target.value)
-        } } />
-        <button onClick={ () => {
-          //filter the restaurant cards & update the UI
-          //searchText
-          console.log(searchText);
-  
-          const filteredRestaurant= listOfRestaurants.filter( (res)=> {res.data.name.toLowerCase().includes(searchText.toLowerCase())} )         // becomes case insensitive now.. YAYY
+              //filter the restaurant cards & update the UI
+              const filteredRestaurant = listOfRestaurants.filter((res) => {
+                res.data.name.toLowerCase().includes(searchText.toLowerCase());
+              }); // becomes case insensitive now.. YAYY
 
-          setlistOfRestaurants(filteredRestaurant);   // updating onto webpage
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
 
-        } }>Search</button>
+        {/*Top Restaurants list */}
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.rating > 4.4,
+            );
+            setlistOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
 
-
-      {/*Top Restaurants list */}
-      <button
-        className="filter-btn"
-        onClick={() => {
-          const filteredList = listOfRestaurants.filter(
-            (res) => res.data.rating > 4.4,
-          );
-          setlistOfRestaurants(filteredList);
-        }}
-      >
-        Top Rated Restaurants
-      </button>
+      <div className="res-container">
+        {/*To rectify the key error */}
+        {setFilteredRestaurant.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant.data.restaurant_id}
+            resData={restaurant}
+          />
+        ))}
+      </div>
     </div>
-    <div className="res-container">
-      {/*To rectify the key error */}
-      {listOfRestaurants.map((restaurant) => (
-        <RestaurantCard
-          key={restaurant.data.restaurant_id}
-          resData={restaurant}
-        />
-      ))}
-    </div>
-  </div>
   );
 };
 
